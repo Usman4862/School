4b245afeffeea02dc020e7d2ec6d40b1b4b3aa3d
Usman4862 <um4862gmail.com>
2023-01-25 14:09:20 -0800

    clear menu

--- functions.py
+++ functions.py
@@ -16,7 +16,7 @@ storage = {
 
 
 def dashboard():
-    # os.system("cls")
+    os.system('cls')
     print("**************ROYAL EDUCATION COMPLEX******************\n")
     print("1: View All Students")
     print("2: Add New Student")
@@ -28,7 +28,7 @@ def dashboard():
 
 def choice_():
     user_choice = input("Enter Your Choice: ")
-    # os.system("cls")
+    # os.system('cls')
     if user_choice == "1":
         view_all_students()
     elif user_choice == "2":
@@ -40,17 +40,20 @@ def choice_():
     elif user_choice == "5":
         search_student()
     elif user_choice == "0":
+        os.system("cls")
         exit()
     else:
-        
+        os.system("cls")
         exit("\nInvalid Choice")
 
 
 def view_all_students():
+    os.system("cls")
     print("**************\n")
     print("All Students \n")
     for roll, values in storage.items():
         print(f"{roll}----{values['firstname']}-----{values['lastname']}-----{values['class']}-----{values['fees']} \n")
+        input("Press enter to go back: ")
 
 
 def add_new_student():
@@ -76,48 +79,55 @@ def add_new_student():
 
 
 def edit_student():
+    os.system("cls")
     print("\n *************")
     print("\nEdit Student")
     _edit = input("Enter Roll Number: ")
-    if _edit == target_:
-        print("1- Edit first name: ")
-        print("2- Edit last name: ")
-        print("3- Edit Roll number: ")
-        print("4- Edit date of birth: ")
-        print("5- Edit class: ")
-        print("6- Edit fees: ")
-        choice = input("Enter Your choice: ")
-        if choice == "1":
-            first_name = input("Enter first name of Student: ")
-            storage[target_]["firstname"] = first_name
-            print(f"\n First name has been change to {first_name}")
-        elif choice == "2":
-            last_name = input("Enter last name of Student: ")
-            storage[target_]["lastname"] = last_name
-            print(f"\n Last name has been change to {last_name}")
-        elif choice == "3":
-            roll_number = input("Enter roll number of Student: ")
-            storage[target_] = roll_number
-            print(f"\n Roll number has been change to {roll_number}")
-        elif choice == "4":
-            date_of_birth = input("Enter date of birth of Student: ")
-            storage[target_]["dateofbirth"] = date_of_birth
-            print(f"\n Date of Birth has been change to {date_of_birth}")
-        elif choice == "5":
-            class_ = input("Enter class of Student: ")
-            storage[target_]["class"] = class_
-            print(f"\n Class has been change to {class_}")
-        elif choice == "6":
-            fees = input("Enter fees of Student: ")
-            storage[target_]["fees"] = fees
-            print(f"\n fees has been change to {fees}")
+    while True:
+        if _edit == target_:
+            print("1- Edit first name: ")
+            print("2- Edit last name: ")
+            print("3- Edit Roll number: ")
+            print("4- Edit date of birth: ")
+            print("5- Edit class: ")
+            print("6- Edit fees: ")
+            print("0- Go Back: ")
+            choice = input("Enter Your choice: ")
+            if choice == "1":
+                first_name = input("Enter first name of Student: ")
+                storage[target_]["firstname"] = first_name
+                print(f"\n First name has been change to {first_name}")
+            elif choice == "2":
+                last_name = input("Enter last name of Student: ")
+                storage[target_]["lastname"] = last_name
+                print(f"\n Last name has been change to {last_name}")
+            elif choice == "3":
+                roll_number = input("Enter roll number of Student: ")
+                storage[target_] = roll_number
+                print(f"\n Roll number has been change to {roll_number}")
+            elif choice == "4":
+                date_of_birth = int(input("Enter date of birth of Student: "))
+                storage[target_]["dateofbirth"] = date_of_birth
+                print(f"\n Date of Birth has been change to {date_of_birth}")
+            elif choice == "5":
+                class_ = input("Enter class of Student: ")
+                storage[target_]["class"] = class_
+                print(f"\n Class has been change to {class_}")
+            elif choice == "6":
+                fees = input("Enter fees of Student: ")
+                storage[target_]["fees"] = fees
+                print(f"\n fees has been change to {fees}")
+            elif choice == "0":
+                break
+            else:
+                os.system("cls")
+                print("Invalid Choice \n")
         else:
-            print("Invalid Choice")
-    else:
-        print("Student not exist")
+            print("Student not exist")
 
 
 def expell_student():
+    os.system("cls")
     print("**************\n")
     print("Remove a Student\n")
     remove = input("Enter Roll Number of a student:")
@@ -126,9 +136,10 @@ def expell_student():
         print(f"{target_} has been removed\n")
     else:
         print("Student not Exists\n")
-
+    input("Press Enter to go back: ")
 
 def search_student():
+    os.system("cls")
     print("**************\n")
     print("------Search a student------- \n ")
     search_ = input("Enter roll number: ")
@@ -139,5 +150,5 @@ def search_student():
     else:
         for roll, values in storage.items():
             print(f"{roll}----{values['firstname']}-----{values['lastname']}-----{values['class']}-----{values['fees']}")
-
+    input("Press Enter to go back: ")
 
